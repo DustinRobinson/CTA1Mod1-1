7045b77792e9e53351a784456d969264b43bf2e2
Dustin Robinson <marshall.robinson@protonmail.com>
2024-02-18 16:41:29 -0700

    initial commit

---
+++ CSC450CTA1Mod1-1/src/CSC450_CT1_mod1-1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+
+//Standard namespace declaration
+using namespace std;
+
+//Main Function
+int main()
+{
+     //Standard Ouput Statement
+     cout << "Welcome to this C++ Program." << endl;
+
+     cout << "I have corrected all errors for this program." << endl;
+
+     // Wait For Output Screen
+     cin.ignore();
+     //Main Function return Statement
+     return 0;
+}
